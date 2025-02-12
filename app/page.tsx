4c3750436c0712a1300fa213a921937d9748d099
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-02-12 17:49:01 +0100

    feat(metadata): add it

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Hero } from '~/components/Landing/Hero/index.tsx';
-import { LatestArticleSection } from '~/components/Landing/LatestArticleSection/index.tsx';
+import { LatestArticleSection } from '~/components/Landing/LastestArticleSection/index.tsx';
 import type { FC } from 'react';
 
 const Page: FC = () => (
