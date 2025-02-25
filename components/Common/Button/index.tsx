3f5ebef63a9c3d4b433b92d4ce8ff512d4d837d8
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-02-25 10:51:55 +0100

    some fixes

--- components/Common/Button/index.tsx
+++ components/Common/Button/index.tsx
@@ -1,9 +1,73 @@
 import classNames from 'classnames';
-import styles from './index.module.css';
 import type { FC, ButtonHTMLAttributes } from 'react';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
 	kind?: 'primary' | 'secondary' | 'special';
+};
+
+const buttonBaseClasses = `
+    rounded-sm
+    inline-flex
+    items-center
+    justify-center
+    gap-2
+    px-4
+    py-2
+    transition
+    duration-300
+    ease-in-out
+    focus:outline-none
+    has-[svg]:*:size-5
+`;
+
+const primaryClasses = `
+    border-2
+    border-solid
+    border-transparent
+    bg-green-500
+    text-white
+    dark:bg-green-400
+    dark:text-gray-900
+    hover:border-gray-600
+    hover:bg-transparent
+    hover:text-gray-600
+    hover:dark:text-gray-300
+    hover:dark:border-gray-300
+`;
+
+const secondaryClasses = `
+    border-2
+    border-solid
+    border-transparent
+    bg-gray-600
+    text-white
+    dark:bg-gray-300
+    dark:text-gray-900
+    hover:border-gray-600
+    hover:bg-transparent
+    hover:text-gray-600
+    hover:dark:text-gray-300
+    hover:dark:border-gray-300
+`;
+
+const specialClasses = `
+    border-2
+    border-solid
+    border-gray-600
+    bg-transparent
+    text-gray-600
+    dark:text-gray-300
+    dark:border-gray-300
+    hover:bg-green-500
+    hover:text-white
+    hover:dark:bg-green-400
+    hover:dark:text-gray-900
+`;
+
+const kindClasses = {
+	primary: primaryClasses,
+	secondary: secondaryClasses,
+	special: specialClasses,
 };
 
 export const Button: FC<ButtonProps> = ({
@@ -14,7 +78,7 @@ export const Button: FC<ButtonProps> = ({
 	...props
 }) => (
 	<button
-		className={classNames(className, styles.button, styles[kind])}
+		className={classNames(buttonBaseClasses, kindClasses[kind], className)}
 		disabled={disabled}
 		aria-disabled={disabled}
 		{...props}
--- components/Landing/LastestArticleSection/index.tsx
+++ components/Landing/LastestArticleSection/index.tsx
@@ -1,12 +1,16 @@
 import { LastestArticle } from '~/components/Post/LastestArticle/index.tsx';
-import styles from './index.module.css';
 import type { FC } from 'react';
 
 export const LatestArticleSection: FC = () => (
-	<section id="lastest-article" className={styles.section}>
-		<div className={styles.header}>
-			<h2 className={styles.title}>Lastes Articles</h2>
-			<p className={styles.p}>
+	<section
+		id="lastest-article"
+		className="bg-green-50 px-10 py-8 dark:bg-green-800"
+	>
+		<div className="mb-8 border-gray-600 border-b-2 dark:border-gray-200">
+			<h2 className="mb-2 font-bold text-3xl dark:text-white">
+				Lastes Articles
+			</h2>
+			<p className="font-semibold text-gray-600 dark:text-gray-200">
 				Discover the lastest articles about Nodejs-Loaders
 			</p>
 		</div>
